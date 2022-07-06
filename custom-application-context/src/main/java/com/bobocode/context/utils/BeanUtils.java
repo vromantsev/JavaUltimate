3a292996753caec90b5e992117aa8182356f9a8c
vromantsev <vromantsev@gmail.com>
2022-07-06 13:45:25 +0300

    added docs, fixed typos

--- custom-application-context/src/main/java/com/bobocode/context/utils/BeanUtils.java
+++ custom-application-context/src/main/java/com/bobocode/context/utils/BeanUtils.java
@@ -6,6 +6,14 @@ public final class BeanUtils {
 
     private BeanUtils() {}
 
+    /**
+     * Resolves bean's name if the bean is annotated via {@link BoboComponent} annotation.
+     * If {@link BoboComponent#name()} parameter has some meaningful value then it will be used as a bean
+     * name, otherwise target class name will be taken with leading char put to lower case (DemoService -> demoService).
+     *
+     * @param targetClass bean class which name should be resolved
+     * @return bean name
+     */
     public static String resolveBeanName(final Class<?> targetClass) {
         final BoboComponent annotation = targetClass.getAnnotation(BoboComponent.class);
         final String name = annotation.name();
