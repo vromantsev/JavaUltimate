87b40b7be8a6543d34786aed95cd1e306a0e08dd
vromantsev <vromantsev@gmail.com>
2022-07-09 13:59:52 +0300

    finished largest NASA picture web service

---
+++ largest-nasa-picture-web-service/src/main/java/com/bobocode/controller/PictureController.java
@@ -0,0 +1,33 @@
+package com.bobocode.controller;
+
+import com.bobocode.domain.PicData;
+import com.bobocode.service.PictureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.bobocode.utils.HttpUtils.uri;
+
+@RestController
+@RequestMapping("/pictures")
+public class PictureController {
+
+    private final PictureService pictureService;
+
+    @Autowired
+    public PictureController(final PictureService pictureService) {
+        this.pictureService = pictureService;
+    }
+
+    @GetMapping("/{sol}/largest")
+    public ResponseEntity<?> getLargestNASAPicture(@PathVariable("sol") final String sol) {
+        final PicData largestPicture = this.pictureService.getLargestPicture(sol);
+        return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT)
+                .location(uri(largestPicture.originalUrl()))
+                .build();
+    }
+}
---
+++ largest-nasa-picture-web-service/src/main/java/com/bobocode/service/PictureService.java
@@ -0,0 +1,9 @@
+package com.bobocode.service;
+
+import com.bobocode.domain.PicData;
+
+public interface PictureService {
+
+    PicData getLargestPicture(final String sol);
+
+}
