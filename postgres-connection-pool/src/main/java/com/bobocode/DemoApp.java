ce7a024cfa72c879c18743249c1a6ced7dd7a647
vromantsev <vromantsev@gmail.com>
2022-08-17 23:31:13 +0300

    implemented driver agnostic connection pool

--- postgres-connection-pool/src/main/java/com/bobocode/DemoApp.java
+++ postgres-connection-pool/src/main/java/com/bobocode/DemoApp.java
@@ -1,7 +1,9 @@
 package com.bobocode;
 
-import com.bobocode.pool.PooledDataSource;
+import com.bobocode.pool.DriverAgnosticPooledDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import lombok.SneakyThrows;
+import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
 
@@ -9,14 +11,24 @@ public class DemoApp {
 
     @SneakyThrows
     public static void main(String[] args) {
-        var dataSource = initializePooledDataSource();
+        final PGSimpleDataSource pgDataSource = new PGSimpleDataSource();
+        pgDataSource.setURL("jdbc:postgresql://localhost:5432/postgres");
+        pgDataSource.setUser("ju22user");
+        pgDataSource.setPassword("ju22pass");
+
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setURL("jdbc:mysql://localhost:3306");
+        mysqlDataSource.setUser("root");
+        mysqlDataSource.setPassword("mysql");
+
+        var dataSource = initializePooledDataSource(mysqlDataSource);
         var total = 0.0;
         var start = System.nanoTime();
         for (int i = 0; i < 50; i++) {
             try (var connection = dataSource.getConnection()) {
                 connection.setAutoCommit(false);
                 try (var statement = connection.createStatement()) {
-                    var rs = statement.executeQuery("select random() from products");
+                    var rs = statement.executeQuery("select rand() from test.products");
                     rs.next();
                     total += rs.getDouble(1);
                 }
@@ -27,11 +39,7 @@ public class DemoApp {
         System.out.println(total);
     }
 
-    private static DataSource initializePooledDataSource() {
-        return new PooledDataSource(
-                "jdbc:postgresql://localhost:5432/postgres",
-                "ju22user",
-                "ju22pass"
-        );
+    private static DataSource initializePooledDataSource(DataSource dataSource) {
+        return new DriverAgnosticPooledDataSource(dataSource);
     }
 }
---
+++ postgres-connection-pool/src/main/java/com/bobocode/pool/NoOperationDataSource.java
@@ -0,0 +1,59 @@
+package com.bobocode.pool;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+public class NoOperationDataSource implements DataSource {
+
+    public NoOperationDataSource() {
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return Logger.getLogger("com.bobocode");
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
+}
