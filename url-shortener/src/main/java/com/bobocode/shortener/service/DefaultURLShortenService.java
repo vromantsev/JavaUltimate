2ecd930733305307cee1e2c5a1d69fa0d0d79e40
vromantsev <vromantsev@gmail.com>
2022-09-26 11:29:59 +0300

    review changes

--- url-shortener/src/main/java/com/bobocode/shortener/service/DefaultURLShortenService.java
+++ url-shortener/src/main/java/com/bobocode/shortener/service/DefaultURLShortenService.java
@@ -4,27 +4,25 @@ import com.bobocode.shortener.dto.URLPayload;
 import com.bobocode.shortener.entity.ShortenedUrl;
 import com.bobocode.shortener.repository.ShortenedUrlRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
-import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
 public class DefaultURLShortenService implements URLShortenService {
 
-    private static final String URL_TEMPLATE = "boboshort.";
     private final ShortenedUrlRepository shortenedUrlRepository;
 
     @Transactional
     @Override
     public String shortenUrl(final URLPayload payload) {
         Objects.requireNonNull(payload);
-        final int randomValue = ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE);
-        var url = URL_TEMPLATE + randomValue;
+        var url = RandomStringUtils.randomAlphanumeric(10);
         var shortenedUrl = ShortenedUrl.builder()
                 .shortUrlId(url)
                 .originalUrl(payload.getUrl())
